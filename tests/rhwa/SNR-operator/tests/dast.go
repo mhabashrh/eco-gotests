17cbe58b568013e7a867cd2512406447f634e0f3
Michael Habash <mhabash@redhat.com~>
2025-11-20 14:29:09 +0200

    add reference to deployment

--- tests/rhwa/SNR-operator/tests/dast.go
+++ tests/rhwa/SNR-operator/tests/dast.go
@@ -14,7 +14,7 @@ import (
 	. "github.com/rh-ecosystem-edge/eco-gotests/tests/rhwa/internal/rhwainittools"
 	"github.com/rh-ecosystem-edge/eco-gotests/tests/rhwa/internal/rhwaparams"
 
-	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe(
@@ -24,8 +24,10 @@ var _ = Describe(
 	Label(farparams.Label), Label("dast"), func() {
 		BeforeAll(func() {
 			By("Verify SNR fence-agents-remediation is the only deployment in namespace")
-			//deploymentList, err := deployment.List(APIClient, rhwaparams.RhwaOperatorNs, metav1.ListOptions{})
+			deploymentList, err := deployment.List(APIClient, rhwaparams.RhwaOperatorNs, metav1.ListOptions{})
 			Expect(err).ToNot(HaveOccurred(), "Failed to list deployments")
+			fmt.Printf("Deployment List: %+v\n", deploymentList)
+
 
 			// if len(deploymentList) != 1 {
 			// 	Skip(fmt.Sprintf("Expected only fence-agents-remediation deployment, found %d deployment(s)", len(deploymentList)))
